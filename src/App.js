cd77ea4d77c15a74859c90b30f1431a7e1920e67
ashu2305 <ayushgupta2305@gmail.com>
2021-12-09 00:17:16 +0530

    Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+
+import {  BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+// Components
+import HomePage from './Components/HomePage/HomePage';
+import Gallery from './Components/Gallery/Gallery';
+import NotFound from './Components/NotFound/NotFound';
+
+const  App =() => {
+  console.log("hello");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+
+          {/* Add your Route here */}
+          <Route  path="/gallery" element={<Gallery />} />
+          <Route  path="/" element={<HomePage />} />
+          <Route exact  path="*" element={<NotFound />} ></Route>
+        </Routes>
+      </Router>
   );
 }
 
---
+++ src/Components/Gallery/Gallery.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const Gallery = () => {
+    
+    return (
+        <div>
+            <h1>Gallery hai ye</h1>
+        </div> 
+    )
+}
+
+export default Gallery;
