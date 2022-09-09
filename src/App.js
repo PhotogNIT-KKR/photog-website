7283cd8148dbf67f4862c53f39c86a9ccec614d6
Kshitiz Pratap Singh <kshitiz100mit@gmail.com>
2022-09-09 17:08:40 +0530

    gallery merged

--- src/App.js
+++ src/App.js
@@ -10,12 +10,9 @@ import Gallery from './Components/Gallery/Gallery';
 import NotFound from './Components/NotFound/NotFound';
 
 const  App =() => {
-  console.log("hello");
   return (
       <Router>
         <Routes>
-
-          {/* Add your Route here */}
           <Route  path="/gallery" element={<Gallery />} />
           <Route  path="/" element={<HomePage />} />
           <Route exact  path="*" element={<NotFound />} ></Route>
