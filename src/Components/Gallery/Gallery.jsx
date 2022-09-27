330b1fd942fc60cda1d74768f978f9ef41ccc92f
Kshitiz Pratap Singh <kshitiz100mit@gmail.com>
2022-09-28 00:13:08 +0530

    Added gradient to gallery

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import locomotiveScroll from "locomotive-scroll";
 import "../../Styles/gallery.css";
 import NavBar from "../NavBar/NavBar";
 import imgArr from "../../utility/img";
 
 const Gallery = () => {
+  const [media, setMedia] = useState([]);
   let images = [];
 
   const showImages = () => {
@@ -27,12 +28,9 @@ const Gallery = () => {
     const func = () => {
       images.forEach((image) => {
         image.addEventListener("click", () => {
-          image.classList.add("-clicked");
-          hideImages();
+          // on click function for media
         });
       });
-
-      setTimeout(showImages(), 1000);
 
       const scroll = new locomotiveScroll({
         el: root,
@@ -49,43 +47,60 @@ const Gallery = () => {
     };
 
     func();
+    setMedia(mediaHandler());
   }, []);
 
-  const setMedia = () => {
+  const mediaHandler = () => {
     let media = [];
 
     for (let i = 0; i < imgArr.length; i++) {
       let speed = Math.floor(Math.random() * 3) + 1;
       let m = (
-        <div className="item -normal" data-scroll data-scroll-speed={speed}>
+        <div className="item -normal" data-scroll data-scroll-speed={speed}  key={"normal2" + i}>
           <img className="image" src={imgArr[i++]} />
         </div>
       );
       media.push(m);
 
+      if (i === imgArr.length) {
+        return media;
+      }
+
       speed = Math.floor(Math.random() * 3) + 1;
       m = (
-        <div className="item -big" data-scroll data-scroll-speed={speed}>
+        <div className="item -big" data-scroll data-scroll-speed={speed} key={"big" + i}>
           <img className="image" src={imgArr[i++]} />
         </div>
       );
       media.push(m);
 
+      if (i === imgArr.length) {
+        return media;
+      }
+
       speed = Math.floor(Math.random() * 3) + 1;
       m = (
-        <div className="item -normal" data-scroll data-scroll-speed={speed}>
+        <div className="item -normal" data-scroll data-scroll-speed={speed} key={"normal1" + i}>
           <img className="image" src={imgArr[i++]} />
         </div>
       );
       media.push(m);
 
+      if (i === imgArr.length) {
+        return media;
+      }
+
       speed = Math.floor(Math.random() * 3) + 1;
       m = (
-        <div className="item -small" data-scroll data-scroll-speed={speed}>
+        <div className="item -small" data-scroll data-scroll-speed={speed} key={"small" + i}> 
           <img className="image" src={imgArr[i++]} />
         </div>
       );
       media.push(m);
+
+      if (i === imgArr.length) {
+        return media;
+      }
 
       speed = Math.floor(Math.random() * 3) + 1;
       m = (
@@ -93,6 +108,7 @@ const Gallery = () => {
           className="item -normal -horizontal"
           data-scroll
           data-scroll-speed={speed}
+          key={"normal3" + i}
         >
           <img className="image" src={imgArr[i++]} />
         </div>
@@ -102,8 +118,6 @@ const Gallery = () => {
 
     return media;
   };
-
-  let media = setMedia();
 
   return (
     <>
