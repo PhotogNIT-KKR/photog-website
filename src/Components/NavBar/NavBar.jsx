dafd2799c0b2041e425dde8cc1022d99def7d258
Kshitiz Pratap Singh <kshitiz100mit@gmail.com>
2022-10-01 01:38:03 +0530

    navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,58 +1,44 @@
-
 import React, { useState } from "react";
 import Mono from "../NavBar/Mono.png";
-import '../../Styles/NavBar.css';
 import { GiHamburgerMenu } from "react-icons/gi";
+import classes from "../../Styles/NavBar.module.css";
 
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [showMediaIcons, setShowMediaIcons] = useState(false);
+  let main_class = [classes.main_container];
+
+  if (showMediaIcons) {
+    main_class.push(classes.sidebar);
+  }
+
+  const navbarHandler = () => {
+    setShowMediaIcons((show) => !show);
+  };
+
   return (
     <>
-      <nav className="main-nav">
-             <div className="logo">
-          <img src={Mono}/>
+      <nav className={main_class.join(" ")}>
+        <div className={classes.logo}>
+          <img src={Mono} />
         </div>
-
-         <div
-          className={
-            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
-          }>
-          <ul>
-            <li>
-              <NavLink to="/">home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Gallery">gallery</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Sponsors">sponsors</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Activities">activities</NavLink>
-            </li>
-            <li className="menu-link1">
-              <NavLink to="/About">about</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Contacts">contact</NavLink>
-            </li>
-            
-          </ul>
+        <div className={classes.navItem1}>
+          <NavLink to="/">home</NavLink>
+          <NavLink to="/Gallery">gallery</NavLink>
+          <NavLink to="/Sponsors">sponsors</NavLink>
+          <NavLink to="/Activities">activities</NavLink>
         </div>
-        
-
-        <div className="social-media">
-          <div className="hamburger-menu">
-            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
-              <GiHamburgerMenu />
-            </a>
-          </div>
+        <div className={classes.navItem2}>
+          <NavLink to="/About">about</NavLink>
+          <NavLink to="/Contacts">contact</NavLink>
         </div>
       </nav>
-
-    
+      <div className={classes.social_media}>
+          <a href="#" onClick={() => navbarHandler()}>
+            <GiHamburgerMenu size={"30px"} />
+          </a>
+        </div>
     </>
   );
 };
